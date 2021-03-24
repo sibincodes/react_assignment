1c9408ee49a89a2ba7e7bf7c18a9d723f625f3fa
Sibin John <sibinjohn92@gmail.com>
2021-03-24 18:40:25 +0530

    Issue resolved

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import { Provider } from "react-redux";
 import reducer from "./Store/Reducers";
 import thunk from 'redux-thunk';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
+
+const store = createStore(reducer);
 
 function App() {
   return (
